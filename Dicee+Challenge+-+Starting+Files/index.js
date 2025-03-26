36e27c84d1dec31f55480aac4705eb57df15c804
Tayyabaankalgi <tayyabaankalgi@gmail.com>
2025-03-26 21:50:47 +0530

    Delete Dicee+Challenge+-+Starting+Files directory

--- Dicee+Challenge+-+Starting+Files/index.js
+++
@@ -1,9 +0,0 @@
-var randomnumber1 = Math.floor(Math.random()*6)+1;
-
-var randomDiceImage = "dice" + randomnumber1 +".png";
-
-var randomeImageSource = "images/"+randomDiceImage;
-
-var image1 = document.querySelectorAll("img")[0];
-
-image1.setAttribute("src", randomImageSource);
